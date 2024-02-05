d0cdaf1c7d804d4b9f9474eb83e9b061138efcf9
Kami <kamarabains@gmail.com>
2024-02-05 16:36:49 +0000

    Adding prettier formatting

--- codegen.ts
+++ codegen.ts
@@ -1,16 +1,16 @@
-import { CodegenConfig } from '@graphql-codegen/cli';
+import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: 'https://rickandmortyapi.com/graphql',
-  documents: ['src/**/*.{ts,tsx}'],
+  schema: "https://rickandmortyapi.com/graphql",
+  documents: ["src/**/*.tsx"],
   generates: {
-    './src/__generated__/': {
-      preset: 'client',
+    "./src/__generated__/": {
+      preset: "client",
       plugins: [],
       presetConfig: {
-        gqlTagName: 'gql',
-      }
-    }
+        gqlTagName: "gql",
+      },
+    },
   },
   ignoreNoDocuments: true,
 };
