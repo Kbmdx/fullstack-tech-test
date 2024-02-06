a4c1a43a45817a97431375f8663123e6e2d73759
Kami <kamarabains@gmail.com>
2024-02-06 13:15:53 +0000

    Changes to container and adding mobile styling

--- src/app/characters/[id]/page.tsx
+++ src/app/characters/[id]/page.tsx
@@ -14,8 +14,8 @@ export default async function CharacterPage({
     query: GET_CHARACTER,
     variables: { id: params.id },
   });
-  
 
+  
   const parsedData = characterDetailsSchema.safeParse(data.character ?? []);
   if (!parsedData.success) {
     console.error(parsedData.error.message);
@@ -26,61 +26,56 @@ export default async function CharacterPage({
 
   return (
     <>
-      <div>
-        <div className="bg-gray-300 bg-fixed w-full h-80">
-          <div className="container mx-auto relative">
-            <h1 className="text-2xl">RICK AND MORTY</h1>
-            <Link className="flex relative" href={`/`}>
-              <button>Back to character listing</button>
-            </Link>
-            <div
-              className="flex relative item-center md:top-[
-              90px] justify-start"
-            >
-              <Image
-                className="rounded-full"
-                src={parsedData.data.image}
-                height={250}
-                width={250}
-                alt="Character image"
-              />
-            </div>
-            <div className="container mx-auto flex flex-col justify-center md:flex-row md:align-center">
-              <div className=" ">
-                <h1 className="text-5xl font-semibold">
-                  {parsedData.data.name}
-                </h1>
-                <p>Status: {parsedData.data.status}</p>
-                <p>Origin:{parsedData.data.origin.name}</p>
-              </div>
-            </div>
-            <div className="pb-6 md:justify-start container mx-auto">
-              <h1 className="text-3xl font-semibold">Location data</h1>
-              <div className="flex justify-between flex-col pb-6">
-                <p>Name:{parsedData.data.location.name}</p>
-                <p>Type:{parsedData.data.location.type}</p>
-                <p>
-                  Dimension: {parsedData.data.origin.dimension ?? "Unknown"}
-                </p>
-                <p>Residents:{parsedData.data.location.residents.length}</p>
-              </div>
-            </div>
-            <div className="pb-6">
-              <h1 className="text-3xl font-semibold">Episodes</h1>
-              <p>First appearance: {episodeArray[0].name}, </p>
-              <p>First air date: {episodeArray[0].air_date}</p>
-              <p>
-                First appearance character count:
-                {parsedData.data.episode[0].characters?.length}
-              </p>
-              <p>Last appearance: {lastEpisode.name} </p>
-              <p>Last air date: {lastEpisode.air_date}</p>
-              <p>
-                First appearance character count:{" "}
-                {lastEpisode.characters?.length}
-              </p>
+      <div className="bg-gray-300 w-full min-h-80">
+        <div className="container md:mx-auto mx-2">
+          <h1 className="text-2xl">RICK AND MORTY</h1>
+          <Link className="flex relative" href={`/`}>
+            <button>Back to character listing</button>
+          </Link>
+          <div className="flex item-center flex-col md:top-[50px] left-0 relative justify-start md:flex-row">
+            <Image
+              className="rounded-full"
+              src={parsedData.data.image}
+              height={250}
+              width={250}
+              alt="Character image"
+            />
+            <div className="justify-center text-center md:text-start flex flex-col md:px-12">
+              <h1 className="text-5xl font-semibold md:pb-8">
+                {parsedData.data.name}
+              </h1>
+              <p >Status: {parsedData.data.status}</p>
+              <p>Origin: {parsedData.data.origin.name}</p>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className="md:justify-start container md:mx-auto mx-2  flex-col flex pt-4 md:pt-24">
+        <div>
+          <h1 className="text-3xl font-semibold">Location data</h1>
+          <div className="flex justify-between flex-col pb-6">
+            <p>Name: {parsedData.data.location.name}</p>
+            <p>Type: {parsedData.data.location.type}</p>
+            <p>Dimension: {parsedData.data.origin.dimension ?? "Unknown"}</p>
+            <p>Residents: {parsedData.data.location.residents.length}</p>
+          </div>
+        </div>
+        <div className="pb-6">
+          <h1 className="text-3xl font-semibold">
+            Episodes: {episodeArray.length}
+          </h1>
+          <p>First appearance: {episodeArray[0].name}, </p>
+          <p>First air date: {episodeArray[0].air_date}</p>
+          <p>
+            First appearance character count:
+            {parsedData.data.episode[0].characters?.length}
+          </p>
+          <p>Last appearance: {lastEpisode.name} </p>
+          <p>Last air date: {lastEpisode.air_date}</p>
+          <p>
+            First appearance character count: {lastEpisode.characters?.length}
+          </p>
         </div>
       </div>
     </>
