de4bd03fd3a28d750349522fc06efd23a6305ace
Kami <kamarabains@gmail.com>
2024-02-06 01:30:24 +0000

    Adding header to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/organisms/TaskIntro";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <Header/>
+        <div className="container mx-auto">{children}</div>
       </body>
     </html>
   );
